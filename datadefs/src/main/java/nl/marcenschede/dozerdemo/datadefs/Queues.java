bf1a7ff8686a8996a39946f61851841022c8c720
Marc Enschede <marc@marcenschede.nl>
2015-03-03 22:19:13 +0100

    App1 en App2 verplaatst naar eigen projecten om ze makkelijker in Jenkins te verwerken. DataDefs opgeruimd en content naar App1 en App2 verplaatst. Lijkt makkelijk; dit is niet relevant voor Docker demo

--- datadefs/src/main/java/nl/marcenschede/dozerdemo/datadefs/Queues.java
+++
@@ -1,9 +0,0 @@
-package nl.marcenschede.dozerdemo.datadefs;
-
-/**
- * Created by marc on 17/01/15.
- */
-public class Queues {
-    
-    public static final String MeldingQueue = "queue/Melding";
-}
