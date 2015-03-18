09a18efc78371938dfbceda8d61adbd8feb4c107
root <root@qbank.summercamp.local>
2015-03-18 22:13:48 +0100

    Wijzigen NGINX configuratie zodat deze werkt op het cluster

--- dockerUI/bin/entrypoint.sh
+++ dockerUI/bin/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-(echo "upstream app1backend { server ${NGINX_APP1HOST:-app1alias:8080}; }" && cat /etc/nginx/conf.d/default.conf) > default.conf.new
-mv default.conf.new /etc/nginx/conf.d/default.conf
+# Gebruik environment variablen om de configuratie van NGINX aan te passen
+sed -i "s/#APP1HOST#/"${NGINX_APP1HOST:-app1alias:8080}"/g" /etc/nginx/conf.d/default.conf
+
+# HOST wordt bepaald door Mesos
+sed -i "s/#PROXYHOST#/"${HOST:-\$host}"/g" /etc/nginx/conf.d/default.conf
 
 exec "$@"
