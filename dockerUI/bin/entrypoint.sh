0788e1087fbc9011759a81088ac996c41f386055
root <root@qbank.summercamp.local>
2015-03-18 22:49:19 +0100

    foutje in entrypoint defaults verbeterd

--- dockerUI/bin/entrypoint.sh
+++ dockerUI/bin/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # Gebruik environment variablen om de configuratie van NGINX aan te passen
-sed -i "s/#APP1HOST#/"${NGINX_APP1HOST:-app1alias:8080}"/g" /etc/nginx/conf.d/default.conf
+sed -i "s/#APP1HOST#/"${NGINX_APP1HOST:-\$host}"/g" /etc/nginx/conf.d/default.conf
 
 # HOST wordt bepaald door Mesos
-sed -i "s/#PROXYHOST#/"${HOST:-\$host}"/g" /etc/nginx/conf.d/default.conf
+sed -i "s/#PROXYHOST#/"${HOST:-app1alias:8080}"/g" /etc/nginx/conf.d/default.conf
+
+cat /etc/nginx/conf.d/default.conf
 
 exec "$@"
